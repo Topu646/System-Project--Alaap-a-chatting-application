b59835e1cb5ea25db4c4a37e09b80674b3717ee7
Proloy51 <proloyphysics323@gmail.com>
2023-09-29 22:44:26 +0600

    Modified in the mainactivity

--- app/src/main/java/com/example/alaap/MainActivity.java
+++ app/src/main/java/com/example/alaap/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.alaap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
     }
 }
