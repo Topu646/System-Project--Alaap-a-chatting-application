140109a79512aa0766aee257d7de2b561c7cece4
Proloy51 <proloyphysics323@gmail.com>
2023-10-06 22:32:05 +0600

    Sign up activity design completed

--- app/src/main/java/com/example/alaap/LoginActivity.java
+++ app/src/main/java/com/example/alaap/LoginActivity.java
@@ -1,14 +1,196 @@
 package com.example.alaap;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button loginButton, signupButton;
+    ImageView googleButton;
+
+    EditText email,password;
+
+    GoogleSignInOptions gso;
+
+    GoogleSignInClient gsc;
+
+    FirebaseAuth mAuth;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        loginButton = findViewById(R.id.loginbutton);
+        signupButton = findViewById(R.id.signupbutton);
+
+        googleButton = findViewById(R.id.googleButton);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2; // Index of drawableEnd
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (password.getRight() - password.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // Handle the click on the drawableEnd here
+                        // For example, toggle the password visibility
+                        togglePasswordVisibility(password);
+                        return true; // Consume the touch event
+                    }
+                }
+                return false; // Let the EditText handle the touch event
+            }
+
+            private void togglePasswordVisibility(EditText password) {
+                if (password.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+            }
+        });
+
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInwithGoogle();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().isEmpty()==true){
+                    email.setError("required");
+                }
+                if(password.getText().toString().isEmpty()==true){
+                    password.setError("password empty");
+                }
+                if(!email.getText().toString().isEmpty() &&  !password.getText().toString().isEmpty()){
+                    signInwithEmailPassword(email.getText().toString(),password.getText().toString());
+                }
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent intent = new Intent(LoginActivity.this,HomeScreen.class);
+//            startActivity(intent);
+//        }
+    }
+
+    private void togglePasswordVisibility(EditText editText) {
+        if (editText.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+            editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        } else {
+            editText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        }
+    }
+
+    private void signOut() {
+        // Implement sign out logic here
+    }
+
+    private void signInwithGoogle() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    void signInwithEmailPassword(String mail,String pass){
+        mAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG,"signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    Intent intent = new Intent(LoginActivity.this,HomeScreen.class);
+                    startActivity(intent);
+                }
+                else{
+                    Log.w(TAG,"signInWithEmail:Failed",task.getException());
+                    Toast.makeText(LoginActivity.this,"Email Or Password is Wrong",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            }
+            catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    private void navigateToSecondActivity() {
+        finish();
+        Intent intent = new Intent(LoginActivity.this,HomeScreen.class);
+        startActivity(intent);
+    }
+
 }
