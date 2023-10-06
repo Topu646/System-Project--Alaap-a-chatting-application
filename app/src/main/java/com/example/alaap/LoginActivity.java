323085c645aa5e08a81499c831836248d416bc74
Proloy51 <proloyphysics323@gmail.com>
2023-10-06 20:26:33 +0600

    Splash screen and login page design done

--- app/src/main/java/com/example/alaap/LoginActivity.java
+++ app/src/main/java/com/example/alaap/LoginActivity.java
@@ -3,17 +3,12 @@ package com.example.alaap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.button);
-
+        setContentView(R.layout.activity_login);
     }
 }
