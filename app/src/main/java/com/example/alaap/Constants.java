6880d7bbf4ec49132640f426322e634d5171c15c
topu <ardamax646@gmail.com>
2023-11-21 21:26:52 +0600

    push notification

--- app/src/main/java/com/example/alaap/Constants.java
+++ app/src/main/java/com/example/alaap/Constants.java
@@ -1,5 +1,7 @@
 package com.example.alaap;
 
+import java.util.HashMap;
+
 public class Constants {
     public final static String KEY_IMAGE = "image";
     public final static String KEY_NAME = "name";
@@ -7,4 +9,25 @@ public class Constants {
     public final static String KEY_FCM_TOKEN = "fcmToken";
     public final static String KEY_COLLECTION_USERS = "users";
     public final static String KEY_USER_ID = "userId";
+    public final static String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+    public static HashMap<String,String>remoteMsgHeaders = null;
+    public static HashMap<String ,String>getRemoteMsgHeaders()
+    {
+        if (remoteMsgHeaders == null)
+        {
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAAQYuCFdU:APA91bHMqHotUJeWgDxq8Owx-aTQkMrXSu4jFm0Rwoy9kkBtXqSDU7kXK3ipwW0sT-85QMVL0sg1zCh4ETUQ0eQdOuBsjeYT__78_M0_JvxSnTxrV86JDDrPgzA4wUByjmowfJD4n06v"
+            );
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_CONTENT_TYPE,
+                    "application/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
--- app/src/main/java/com/example/alaap/HomeScreen.java
+++ app/src/main/java/com/example/alaap/HomeScreen.java
@@ -210,12 +210,12 @@ public class HomeScreen extends BaseActivity implements ConversationListener{
     }
     private void updateToken(String token)
     {
+        preferenceManager.putString(Constants.KEY_FCM_TOKEN,token);
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
                 preferenceManager.getString(Constants.KEY_USER_ID)
         );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
